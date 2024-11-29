26ea8a68ccc76f4c49bf8b6d654777e9be12c461
Evilvir-us <jeff@evilvir.us>
2024-11-29 05:24:00 -0800

    Icon added

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-pyinstaller --onefile --add-data "templates/*;templates" --add-data "static/*;static" --add-data "ffmpeg/ffmpeg.exe;." --add-data "ffmpeg/ffprobe.exe;." --hidden-import=stb --hidden-import=waitress app.py
+pyinstaller --onefile --add-data "templates/*;templates" --icon=icon.ico --add-data "static/*;static" --add-data "ffmpeg/ffmpeg.exe;." --add-data "ffmpeg/ffprobe.exe;." --hidden-import=stb --hidden-import=waitress app.py
 copy dist\app.exe .\MacSurge.exe
 pause
