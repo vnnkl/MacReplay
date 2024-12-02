d16d99d41850b8d2e1e6e1fe7be9ca9ba4a1ded8
Evilvirus <jeff@evilvir.us>
2024-12-02 15:08:52 -0800

    Update build.bat
    fixed a typo

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-pyinstaller --onefile --add-data "templates/*;templates" --icon=replay.ico --add-data "static/*;static" --add-data "ffmpeg/*ffmpeg.exe;ffmpeg" --hidden-import=stb --hidden-import=waitress app.py
+pyinstaller --onefile --add-data "templates/*;templates" --icon=replay.ico --add-data "static/*;static" --add-data "ffmpeg/;ffmpeg" --hidden-import=stb --hidden-import=waitress app.py
 copy dist\app.exe .\MacReplay.exe
 pause
